54bae7e87d0a41aefe5b864b6638769d29c37eba
ATOM03 <priyanshujhaginia@outlook.com>
2024-04-12 01:46:25 +0530

    setup the backend

---
+++ src/app/api/route.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+
+export async function GET(req: Request){
+  
+  try{
+    const {data} = await axios.get('https://api.covid19india.org/data.json')
+    // console.log(data);
+    return NextResponse.json({ data: data }, { status: 200 })
+  }
+  catch(error){
+    console.log(error)
+    return NextResponse.json({ error: error }, { status: 500 })
+  }
+    // await axios.get('https://api.covid19india.org/data.json')
+    //   .then(function (response) {
+    //     // console.log(response.data);
+    //     NextResponse.json({ data: response.data }, { status: 200 })
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+}
