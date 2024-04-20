9ba52bfd33ae80bf4c8f2687686f65c4fcd16137
ATOM03 <priyanshujhaginia@outlook.com>
2024-04-20 20:20:36 +0530

    Fixed the recoid production Issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { RecoilRoot } from "recoil";
+import RecoidContextProvider from "./recoilContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <RecoidContextProvider>{children}</RecoidContextProvider>
+      </body>
     </html>
   );
 }
