1a67b7ec38363669d6aee423707181896a3c062c
ATOM03 <priyanshujhaginia@outlook.com>
2024-04-24 22:29:20 +0530

    added the recovered cases

--- src/app/components/Chart/page.tsx
+++ src/app/components/Chart/page.tsx
@@ -27,17 +27,28 @@ export default function Chart(props: any) {
     let totalConfimed = {
       label: "Total Confirmed Cases",
       data: [],
+      borderColor: "rgb(255, 7, 58)",
+      pointBackgroundColor: "rgb(255, 7, 58)",
+      hoverBorderColor: "rgb(255, 7, 58)",
+      pointHoverRadius: 5,
     };
 
     let totalDeaths = {
       label: "Total Deaths",
       data: [],
+      borderColor: "rgba(200,200,200,0.6)",
+      pointBackgroundColor: "rgba(200, 200, 200, 1)",
+      hoverBorderColor: "rgba(230,230,230,1)",
+      pointHoverRadius: 6,
     };
 
     let totalRecovered = {
       label: "Total Recovered",
       data: [],
       borderColor: "rgba(0,255,0, 0.6)",
+      pointBackgroundColor: "rgba(0, 255, 0, 0.8)",
+      hoverBorderColor: "rgba(0,255,0,0.9)",
+      pointHoverRadius: 6,
     };
 
     let chartData = chartdata;
@@ -53,8 +64,8 @@ export default function Chart(props: any) {
     }
 
     data.confirmedCase.datasets.push(totalConfimed as never);
-    data.recoveredCase.datasets.push(totalDeaths as never);
-    data.deathCase.datasets.push(totalRecovered as never);
+    data.deathCase.datasets.push(totalDeaths as never);
+    data.recoveredCase.datasets.push(totalRecovered as never);
 
     console.log(data);
     return data;
@@ -64,28 +75,44 @@ export default function Chart(props: any) {
   console.log(data.confirmedCase);
 
   return (
-    <div style={{ width: "100%" }}>
+    <div className="w-full ">
       <Line
         data={data.confirmedCase}
         options={{
           responsive: true,
-          //   layout: {
-          //     padding: {
-          //       top: 32,
-          //       bottom: 32,
-          //       left: 10,
-          //     },
-          //   },
-          //   maintainAspectRatio: false,
-          //   title: {
-          //     display: true,
-          //     text: "STATUS",
-          //     fontSize: 25,
-          //   },
-          //   legend: {
-          //     display: true,
-          //     position: "top",
-          //   },
+          layout: {
+            padding: {
+              top: 32,
+              bottom: 32,
+              left: 10,
+            },
+          },
+        }}
+      />
+      <Line
+        data={data.recoveredCase}
+        options={{
+          responsive: true,
+          layout: {
+            padding: {
+              top: 32,
+              bottom: 32,
+              left: 10,
+            },
+          },
+        }}
+      />
+      <Line
+        data={data.deathCase}
+        options={{
+          responsive: true,
+          layout: {
+            padding: {
+              top: 32,
+              bottom: 32,
+              left: 10,
+            },
+          },
         }}
       />
     </div>
